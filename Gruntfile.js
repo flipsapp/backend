7b4c592673708fb21aa6a0ac2e31609f2b7fe150
arctouch-ecilteodoro <ecil.teodoro@arctouch.com>
2014-09-09 16:29:36 -0300

    Test framework configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,22 @@ module.exports = function(grunt) {
 	}
 
 
+// Add the grunt-mocha-test tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
 
+  grunt.initConfig({
+    // Configure a mochaTest task
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['tests/*.js']
+      }
+    }
+  });
+
+  grunt.registerTask('test', 'mochaTest');
 
 	// Load task functions
 	var taskConfigurations = loadTasks('./tasks/config'),
