d4abfb0eba42dc0acf36a9783d7def4b4a44fc34
Diego Santiviago <diego.santiviago@arctouch.com>
2014-09-11 15:59:59 -0300

    Fixing owner policy
    Including MugError as a response body

--- api/policies/owner.js
+++ api/policies/owner.js
@@ -2,7 +2,7 @@ module.exports = function (request, response, next) {
   var owner = request.params.parentid || request.params.id;
 
   if (!request.user) {
-    return response.send(403, 'No user in session. Please, authenticate first.');
+    return response.send(403, new MugError('No user in session.', 'Please, authenticate first.'));
   }
 
   if (owner == request.user.id) {
