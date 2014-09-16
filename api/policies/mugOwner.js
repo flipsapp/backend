e96f372f7f68d39f397a3c74800701ae9fedae8a
arctouch-ecilteodoro <ecil.teodoro@arctouch.com>
2014-09-16 15:41:44 -0300

    Fix to catch exception.

--- api/policies/mugOwner.js
+++ api/policies/mugOwner.js
@@ -3,6 +3,10 @@ var MugError = requires('>/api/utilities/MugError');
 module.exports = function (request, response, next) {
   var userId = request.params.user_id;
 
+  if (!request.user) {
+    return response.send(403, new MugError('User must login first'));
+  }
+
   if (userId == request.user.id) {
     return next();
   }
