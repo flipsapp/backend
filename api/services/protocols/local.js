d40f381cc5c8ee6b9a6fc7418ae5b63c289b3cd2
Diego Santiviago <diego.santiviago@arctouch.com>
2014-10-09 10:52:58 -0300

    Fixing comma position

--- api/services/protocols/local.js
+++ api/services/protocols/local.js
@@ -82,8 +82,8 @@ exports.login = function (req, identifier, password, next) {
     }
 
     Passport.findOne({
-      protocol : 'local'
-    , user     : user.id
+      protocol : 'local',
+      user     : user.id
     }, function (err, passport) {
       if (passport) {
         passport.validatePassword(password, function (err, res) {
@@ -120,9 +120,9 @@ exports.createUser = function(userModel, next) {
       }
 
       Passport.create({
-        protocol : 'local'
-        , password : userModel.password
-        , user     : user.id
+        protocol : 'local',
+        password : userModel.password,
+        user     : user.id
       }, function (err, passport) {
         if (err) {
           if (err.code === 'E_VALIDATION') {
