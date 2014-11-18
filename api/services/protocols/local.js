4297a49a971d152951e6a8c7aa9f34fb2dc46775
Ecil Teodoro <ecil.teodoro@arctouch.com>
2014-11-18 11:55:18 -0800

    Uploading photo

--- api/services/protocols/local.js
+++ api/services/protocols/local.js
@@ -305,11 +305,12 @@ var createPassportAndInitialRoom = function (user, password, photo, next) {
               logger.debug('11.1 populatedUser: ' + populatedUser.username);
 
               var uploadedFile = uploadedFiles[0];
-              logger.debug(uploadedFile)
+              var a = uploadedFile
+              logger.debug(JSON.stringify(a));
 
-              logger.debug('11.2 file location: ' + uploadedFile.extra.Location);
+              logger.debug('11.2 file location: ' + 'https://s3.amazonaws.com/' + s3service.PICTURES_BUCKET + '/' + uploadedFile.fd);
 
-              populatedUser.photoUrl = uploadedFile.extra.Location;
+              populatedUser.photoUrl = 'https://s3.amazonaws.com/' + s3service.PICTURES_BUCKET + '/' + uploadedFile.fd;
               populatedUser.save(function (saveErr) {
                 if (saveErr) {
                   logger.error('DB Error when trying to save user');
