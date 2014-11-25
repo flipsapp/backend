19b3e87f71aa29ea3331324f036d9a5a6dd039ec
Ecil Teodoro <ecil.teodoro@arctouch.com>
2014-11-25 14:19:05 -0800

    Twilio phone number pool

--- api/services/twilioService.js
+++ api/services/twilioService.js
@@ -1,14 +1,33 @@
 var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-var from = '+18587035477';
 
 var TwilioService = {
 
-  sendSms: function(to, message, callback) {
-    client.messages.create({
-      body: message,
-      to: to,
-      from: from
-    }, callback);
+  sendSms: function (to, message, callback) {
+    this.getNumberFromPool(function(err, fromNumber) {
+      if (err) {
+        return callback(err);
+      } else {
+        client.messages.create({
+          body: message,
+          to: to,
+          from: fromNumber
+        }, callback);
+      }
+    });
+
+  },
+
+  getNumberFromPool: function (callback) {
+    client.incomingPhoneNumbers.list(function (err, data) {
+      if (err || !data) {
+        return callback(err);
+      } else {
+        var phoneNumbersCount = data.incomingPhoneNumbers.length;
+        var index = Math.floor(Math.random() * phoneNumbersCount);
+        return callback(null, data.incomingPhoneNumbers[index].phone_number);
+      }
+
+    });
   }
 };
 
