3d1b063c075494b5b6503de4acc5241eb7d60452
Alex Magalhaes <alex.magalhaes@arctouch.com>
2015-03-24 17:45:23 -0300

    Saving the profile picture every time a temporary user is updated.

--- api/services/protocols/local.js
+++ api/services/protocols/local.js
@@ -180,6 +180,24 @@ exports.createUser = function (userModel, next) {
           }
           tempUser.birthday = userModel.birthday;
 
+          if (photo && photo._files.length >= 1) {
+            s3service.upload(photo, s3service.PICTURES_BUCKET, function (err, uploadedFiles) {
+              if (err) {
+                var errmsg = new FlipsError('Error uploading picture', err);
+                logger.error(errmsg);
+                return response.send(500, errmsg);
+              }
+
+              if (!uploadedFiles || uploadedFiles.length < 1) {
+                return response.send(400, new FlipsError('Error uploading file'));
+              }
+
+              var uploadedFile = uploadedFiles[0];
+              tempUser.photoUrl = s3service.S3_URL + s3service.PICTURES_BUCKET + '/' + uploadedFile.fd;
+              tempUser.save();
+            });
+          }
+
           tempUser.save(function (err) {
             if (err) {
               logger.debug(err);
@@ -188,7 +206,7 @@ exports.createUser = function (userModel, next) {
             }
 
             User.findOne(tempUser.id).exec(function(error, user) {
-              if (err || !user) {
+              if (error || !user) {
                 logger.debug('It was not possible to sign up this user');
                 return next('It was not possible to sign up this user');
               }
