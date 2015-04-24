f3f8d3a774d732002a38a7ec5f860f75c13df1b9
Ecil Teodoro <ecil.teodoro@arctouch.com>
2015-04-24 16:35:35 -0700

    Create password for flipboys

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -22,25 +22,53 @@ module.exports.bootstrap = function(cb) {
     lastName: ' ',
     photoUrl: 'https://s3.amazonaws.com/flips-pictures/flipboys_avatar.png',
     birthday: '1970-01-01',
-    phoneNumber: '+14155555555'
-  }).exec(function(err, user) {
-    User.create({
-      username: process.env.STOCKFLIPS_USERNAME,
-      firstName: 'Stock',
-      lastName: 'Flips',
-      photoUrl: 'https://s3.amazonaws.com/flips-pictures/flipboys_avatar.png',
-      birthday: '1970-01-01',
-      phoneNumber: '+14155555556'
-    }).exec(function(err, user) {
-      if (user) {
-        Passport.create({
-          protocol: 'local', password: process.env.STOCKFLIPS_PASSWORD, user: user.id
-        }, function (passportError, passport) {
-          cb();
+    phoneNumber: '+14155555555',
+    isTemporary: false
+  }).exec(function(err, flipboysUser) {
+    if (flipboysUser) {
+      Passport.create({
+        protocol: 'local', password: process.env.FLIPBOYS_PASSWORD, user: flipboysUser.id
+      }, function (passportError, passport) {
+        User.create({
+          username: process.env.STOCKFLIPS_USERNAME,
+          firstName: 'Stock',
+          lastName: 'Flips',
+          photoUrl: 'https://s3.amazonaws.com/flips-pictures/flipboys_avatar.png',
+          birthday: '1970-01-01',
+          phoneNumber: '+14155555556',
+          isTemporary: false
+        }).exec(function(err, stockflipsUser) {
+          if (stockflipsUser) {
+            Passport.create({
+              protocol: 'local', password: process.env.STOCKFLIPS_PASSWORD, user: stockflipsUser.id
+            }, function (passportError, passport) {
+              cb();
+            });
+          } else {
+            cb();
+          }
         });
-      } else {
-        cb();
-      }
-    });
+      });
+    } else {
+      User.create({
+        username: process.env.STOCKFLIPS_USERNAME,
+        firstName: 'Stock',
+        lastName: 'Flips',
+        photoUrl: 'https://s3.amazonaws.com/flips-pictures/flipboys_avatar.png',
+        birthday: '1970-01-01',
+        phoneNumber: '+14155555556'
+      }).exec(function(err, stockflipsUser) {
+        if (stockflipsUser) {
+          Passport.create({
+            protocol: 'local', password: process.env.STOCKFLIPS_PASSWORD, user: stockflipsUser.id
+          }, function (passportError, passport) {
+            cb();
+          });
+        } else {
+          cb();
+        }
+      });
+    }
+
   });
 };
